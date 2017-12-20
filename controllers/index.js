9c7cffb73d57621c964f6ee9b3fbbb7a68e6aa77
Vitali Vorobyev <vvitali26@gmail.com>
2017-12-20 13:04:43 -0500

    config.json - switched to local DB

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+var htmlRoutes = require("./html_routes.js");
+var apiRoutes = require("./api_routes.js");
+
+module.exports = {
+	apiRoutes: apiRoutes,
+	htmlRoutes: htmlRoutes
+
+}
--- server.js
+++ server.js
@@ -3,18 +3,20 @@ var app = express();
 var handlebars = require("express-handlebars");
 
 //connectiong handlebars engine to express server
-app.use("view engine", "handlebars");
+app.set("view engine", "handlebars");
 var path = require("path");
 
 //making "public" folder realy public for client side
-app.set(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //connecting bodyparsers to express server
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-var PORT = process.env.PORT || 8080;
+var controllers = require("./controllers");
 
+
+var PORT = process.env.PORT || 8080;
 app.listen(PORT, function(){
 	console.log("Server started at "+PORT);
 })
