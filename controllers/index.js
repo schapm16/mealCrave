3d082797814664c2b94a577bda20d71913910893
Vitali Vorobyev <vvitali26@gmail.com>
2017-12-20 15:09:46 -0500

    encoding, decoding base64 images

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,30 @@
 console.log("Controllers: Ok!");
+var path = require('path'),
+fs = require('fs');
+//express part for uploading files from html-form
+var multer  = require('multer');
+var upload = multer({ dest: null });
+
 module.exports = function(app){
 
 	app.get("/", function(request, response){
 		console.log("index requested");
 		response.render("login");
 	});
+	app.post('/upload', upload.single('file'), function (request, response) {
+		console.log("Post Upload Photo request!");
+		console.log(!!request.file);
+		console.log(typeof(request.file));
+		console.log(request.file)
+
+		var image = new Buffer(request.file.buffer).toString("base64");
+
+		fs.writeFile("test.txt", image, function(){
+			console.log("Done!");
+		})
+
+	});
 
 }
+
+
