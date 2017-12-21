df518d03621893f78ae76f4904433289be2bee0e
Jonathan Boesel <jboesel@vt.edu>
2017-12-21 18:31:53 +0000

    google route

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,23 @@
 console.log("Controllers: Ok!");
-module.exports = function(app){
+var db = require("../models");
 
-	app.get("/", function(request, response){
+module.exports = function(app) {
+
+	app.get("/", function(request, response) {
 		console.log("index requested");
 		response.render("login");
 	});
 
-}
+	app.get("/api/location/:id", function(req, res) {
+		db.Locations.findOne({
+			where: {
+				locations_id: req.params.id
+			}
+		}).then(function(location) {
+			res.json(location)
+		})
+	});
+
+
+
+};
