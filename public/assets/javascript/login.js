b531abe33784de4160ec479f2ef57772a8b73e94
Stephen Chapman <schapm16@gmail.com>
2017-12-23 14:11:29 +0000

    Add to user login/join input validation

--- public/assets/javascript/login.js
+++ public/assets/javascript/login.js
@@ -1,32 +1,38 @@
 // >>> Start Function Declarations <<<
 // Updates page with profile link once user is logged in
 function loggedIn(userName) {
-  
+
 
 }
 
 function loginValidation(userName, password) {
-  
-  $.ajax({
+
+  if (userName === '' || password === '') {
+    $('#loginError').html('<p>Please fill out all of the fields</p>');
+  }
+  else {
+    $.ajax({
       method: 'GET',
       traditional: true,
-      url: '/login', //This is a pending route, confirm with backend team
+      url: '/login',
       data: { userName: userName, password: password }
     }).done(function(response) {
-      // How is backend going to respond?
       if (response.valid === true) {
         console.log('Login Successful');
         $('#loginModal').removeClass('is-active');
-        loggedIn(userName);
+        loggedIn(userName); // Login user once userName and password are verified
       }
     });
-  
+  }
 }
 
 function joinValidation(userName, passwordOne, passwordTwo) {
 
-  if (passwordOne !== passwordTwo) {
-    $("#joinError").html("<p>Passwords Do Not Match</p>");
+  if (userName === '' || passwordOne === '' || passwordTwo === '') {
+    $('#joinError').html('<p>Please fill out all of the fields</p>');
+  }
+  else if (passwordOne !== passwordTwo) {
+    $('#joinError').html('<p>Passwords Do Not Match</p>');
   }
   else {
 
@@ -36,18 +42,17 @@ function joinValidation(userName, passwordOne, passwordTwo) {
       url: "/join",
       data: { userName: userName, password: passwordOne }
     }).done(function(response) {
-      
-      if(response.valid === false) {
+
+      if (response.valid === false) {
         $("#joinError").html("<p>Username Already Existis</p>");
       }
-      
+
       else {
-      // Login user once new account is verified
-      console.log('Join Succesful');
-      $('#joinModal').removeClass('is-active');
-      loggedIn(userName);
+        console.log('Join Succesful');
+        $('#joinModal').removeClass('is-active');
+        loggedIn(userName); // Login user once new account is verified
       }
-      
+
     });
   }
 }
@@ -76,16 +81,18 @@ $(function() {
     $('#loginModal').addClass('is-active');
   });
 
-  // Closes login modal upon click of 'X'
+  // Closes login modal and clears form fields upon click of 'X'
   $('#closeLoginModal').click(function() {
     $('#loginModal').removeClass('is-active');
+    $('#loginUsername').val('');
+    $('#loginPassword').val('');
   });
 
   // Takes user login information and sends to server for confirmation
   $('#loginButton').click(function() {
     var userName = $('#loginUsername').val().trim();
     var password = $('#loginPassword').val().trim();
-    
+
     loginValidation(userName, password);
   });
   // >>> End Login Modal <<<
@@ -93,13 +100,16 @@ $(function() {
 
   // >>> The following is for the Join Modal <<<
   // Opens modal upon click of Join link
-  $('#join').on("click",function() {
+  $('#join').on("click", function() {
     $('#joinModal').addClass('is-active');
   });
 
-  // Closes login modal upon click of 'X'
+  // Closes join modal and clears form fields upon click of 'X'
   $('#closeJoinModal').click(function() {
     $('#joinModal').removeClass('is-active');
+    $('#joinUsername').val('');
+    $('#joinPasswordOne').val('');
+    $('#joinPasswordTwo').val('');
   });
 
   $('#joinButton').click(function() {
