aab3e1004dd13dc437702b016857e335987f2698
Stephen Chapman <schapm16@gmail.com>
2017-12-30 00:21:30 +0000

    Initial Commit of profile page

---
+++ public/assets/javascript/profile.js
@@ -0,0 +1,45 @@
+/*global $*/
+$(function() {
+  $('#logout').click(function() {
+    sessionStorage.clear();
+    window.location.href='/';
+  });
+  
+  // Form Controls
+  $('#openCreateDishForm').click(function() {
+    $('#initialButtons').css('display', 'none');
+    $('#createDishForm').css('display', 'block');
+  });
+  
+  $('#openSearchForm').click(function(){
+    $('#initialButtons').css('display', 'none');
+    $('#searchForm').css('display', 'block');
+  });
+
+  $('.closeForm').click(function(){
+    $('#createDishForm').css('display', 'none');
+    $('#searchForm').css('display', 'none');
+    $('#initialButtons').css('display', 'block');
+  });
+  // End Form Controls
+  
+  
+  // Get userName and Location on Page load
+  $('#userName').val(sessionStorage.getItem('userName'));
+  
+  window.navigator.geolocation.getCurrentPosition(function(position){
+    console.log('position attained');
+    $('#locationCoordinates').val(position.coords.latitude + ' ' + position.coords.longitude);
+    console.log($('#locationCoordinates').val());
+  }, null, {enableHighAccuracy: true, maximumAge: 300000, timeOut: 5000});
+  // End Get userName and Location on Page Load
+  
+  // Search by Keyword
+  $('#search').click(function(){
+    window.location.href="/search/byKeyword/" + $('#searchTerm').val().trim();
+  });
+  // End Search by Keyword
+
+  
+
+});
