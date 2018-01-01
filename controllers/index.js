fd3336619b90227f7f9eddfeb921d80e9f55a2c3
Jonathan Boesel <jboesel@vt.edu>
2018-01-01 23:47:51 +0000

    edit and delete

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 var DEBUG = true;
 console.log("Controllers: \x1b[32mok!\x1b[0m");
 var path = require('path'),
-fs = require('fs');
+	fs = require('fs');
 //express part for uploading files from html-form
 var multer = require('multer');
 //var S3 = require("../models/amazon.js");
@@ -10,31 +10,31 @@ var upload = multer({ dest: null });
 
 console.log(Object.getOwnPropertyNames(DB));
 
-module.exports = function(app){
-	app.get("/", function(request, response){
+module.exports = function(app) {
+	app.get("/", function(request, response) {
 		console.log("index requested");
-		response.render("login", {stylePath: '"./assets/css/login.css"'});
+		response.render("login", { stylePath: '"./assets/css/login.css"' });
 	});
-	app.get("/addFood", function(request, response){
+	app.get("/addFood", function(request, response) {
 		console.log("index requested");
 		response.render("addFood");
 	});
 	//add only food element to the database
-	app.post("/api/addFood", upload.single('photo'), function(request, response){
+	app.post("/api/addFood", upload.single('photo'), function(request, response) {
 		//request.file - this variable will contain file from "photo" key
 		var food = {
 			user: ""
 		}
-		DEBUG && console.log(request.file.originalname);		
+		DEBUG && console.log(request.file.originalname);
 
 		DB.sendFoodToDB(request.body.menuName, //+
-			request.body.userName,//+
-			request.file, 
+			request.body.userName, //+
+			request.file,
 			request.body.price, //+
 			request.body.location, //+
 			request.body.gFree, //+
 			request.body.veg, //+
-			request.body.type, 
+			request.body.type,
 			request.body.tags);
 	});
 
@@ -54,7 +54,7 @@ module.exports = function(app){
 				type: req.params.type
 			}
 		}).then(function(data) {
-			DEBUG || console.log("Poutput:"+data);
+			DEBUG || console.log("Poutput:" + data);
 			var JSON = data.stringify();
 			res.render("searchResults", { data: data, stylePath: "assets/css/searchResults.css" });
 		});
@@ -63,13 +63,13 @@ module.exports = function(app){
 	//and will send a JSON back
 	app.get("/search/byKeyword/:keyword", function(request, response) {
 		DB.Food.findAll({
-			where: {				
+			where: {
 				food_name: {
 					$like: '%' + request.params.keyword + '%' //it will find every item with "keyword" in the food_name column, no matter what position
 				}
 			}
 		}).then(function(data) {
-			DEBUG || console.log("Poutput:"+data);
+			DEBUG || console.log("Poutput:" + data);
 			response.render("searchResults", {
 				stylePath: '"/assets/css/searchResults.css"',
 				data: data
@@ -79,12 +79,12 @@ module.exports = function(app){
 	//this function will find every row in Food table from certain user, it uses user_id for searching
 	app.get("/search/byUserId/:userId", function(request, response) {
 		DB.Food.findAll({
-			where: {				
+			where: {
 				user_id: request.params.userId
 			}
 		}).then(function(data) {
-			DEBUG || console.log("Poutput:"+data);
-			//data will contain an array of food objects, each object contains has same keys as columns inside food-table in mysql-db 
+			DEBUG || console.log("Poutput:" + data);
+			//data will contain an array of food objects, each object contains has same keys as columns inside food-table in mysql-db
 			response.render("profile", {
 				stylePath: '"/assets/css/profile.css"',
 				usersFood: data
@@ -100,7 +100,7 @@ module.exports = function(app){
 				login: req.body.userName
 			}
 		}).then(function(data) {
-			DEBUG && console.log("Join: response: "+!data);
+			DEBUG && console.log("Join: response: " + !data);
 			if (!data) {
 				DB.Users.create({
 					login: req.body.userName,
@@ -116,7 +116,7 @@ module.exports = function(app){
 	});
 
 	app.post("/login", function(req, res) {
-		DEBUG && console.log("\x1b[33m"+"Login attempt:\nLogin: "+ req.body.login+"\nPassword: "+req.body.password+"\x1b[0m");
+		DEBUG && console.log("\x1b[33m" + "Login attempt:\nLogin: " + req.body.login + "\nPassword: " + req.body.password + "\x1b[0m");
 		DB.Users.findOne({
 			where: {
 				login: req.body.userName
@@ -124,13 +124,32 @@ module.exports = function(app){
 		}).then(function(data) {
 
 			if (data.password == req.body.password) {
-				DEBUG && console.log("\x1b[32m"+req.body.login+": Access granted!"+"\x1b[0m");
+				DEBUG && console.log("\x1b[32m" + req.body.login + ": Access granted!" + "\x1b[0m");
 				res.json({ valid: true })
 			}
 			else {
-				DEBUG && console.log("\x1b[31m"+req.body.login+": Access denied!"+"\x1b[0m");
+				DEBUG && console.log("\x1b[31m" + req.body.login + ": Access denied!" + "\x1b[0m");
 				res.json({ valid: false })
 			}
 		})
 	});
-}
+
+	app.post("/api/updateFood", function(req, res) {
+		DB.editFoodInDB(req.body.foodId,
+			req.body.location,
+			req.body.price,
+			req.body.menuName,
+			req.body.veg,
+			req.body.gfree,
+			req.body.file
+		).then(function() {
+			res.redirect("/search/byUserId/" + req.body.userName)
+		});
+	});
+
+	app.post("api/deleteFood", function(req, res) {
+		DB.deleteFood(req.body.foodId).then(function() {
+			res.redirect("/search/byUserId/" + req.body.userName)
+		});
+	})
+};
--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ var basename = path.basename(__filename);
 var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/../config/config.json')[env];
 var db = {};
-var S3 = require("./amazon2.js")(); 
+var S3 = require("./amazon2.js")();
 if (config.use_env_variable) {
 	var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 }
@@ -23,15 +23,15 @@ const Users = sequelize.define('users', {
 		primaryKey: true,
 		autoIncrement: true,
 	},
-	login:  {
+	login: {
 		type: Sequelize.STRING,
 		unique: true,
-		allowNull: false 
+		allowNull: false
 	},
 	alias: Sequelize.STRING,
 	password: {
 		type: Sequelize.STRING,
-		allowNull: false 
+		allowNull: false
 	},
 	preferences: Sequelize.STRING,
 });
@@ -43,15 +43,15 @@ const Food = sequelize.define('food', {
 	},
 	food_name: {
 		type: Sequelize.STRING,
-		allowNull: false 
+		allowNull: false
 	},
 	photoUrl: {
 		type: Sequelize.STRING,
-		allowNull: false 
+		allowNull: false
 	},
 	price: {
 		type: Sequelize.FLOAT,
-		allowNull: false 
+		allowNull: false
 	},
 	gluFree: Sequelize.BOOLEAN,
 	type: Sequelize.STRING,
@@ -60,75 +60,74 @@ const Food = sequelize.define('food', {
 const Locations = sequelize.define('locations', {
 	location_name: {
 		type: Sequelize.STRING,
-		allowNull: false 
+		allowNull: false
 	},
 	gps_tag: {
 		type: Sequelize.STRING,
-		allowNull: false 
+		allowNull: false
 	},
 });
 
 Food.belongsTo(Locations);
 Users.belongsTo(Locations);
-Food.belongsTo(Users, {foreignKey: "user_id"});
+Food.belongsTo(Users, { foreignKey: "user_id" });
 
 sequelize.sync()
-.then(() => {	
-}).then(()=>{
-	console.log("Synced!");
-	//if argument was passed in command linu at start - create a test-data in database
-	if(process.argv[2]){
+	.then(() => {}).then(() => {
+		console.log("Synced!");
+		//if argument was passed in command linu at start - create a test-data in database
+		if (process.argv[2]) {
 
-		Locations.create({
-			location_name:"Charlotte, NC",
-			gps_tag: "34.333, 35.222"
-		});
-		Locations.create({
-			location_name:"Portland, OR",
-			gps_tag: "66.77, 12.192"
-		});
-		Users.create({
-			login: "vvitali",
-			alias: "Vorobyev",
-			password: "test_password",
-			locationId: 1,
-			preferences: "no",
-		});
-		Users.create({
-			login: "cstephen",
-			alias: "Chapman",
-			password: "s_password_test",
-			locationId: 2,
-			preferences: "no",
-		});
+			Locations.create({
+				location_name: "Charlotte, NC",
+				gps_tag: "34.333, 35.222"
+			});
+			Locations.create({
+				location_name: "Portland, OR",
+				gps_tag: "66.77, 12.192"
+			});
+			Users.create({
+				login: "vvitali",
+				alias: "Vorobyev",
+				password: "test_password",
+				locationId: 1,
+				preferences: "no",
+			});
+			Users.create({
+				login: "cstephen",
+				alias: "Chapman",
+				password: "s_password_test",
+				locationId: 2,
+				preferences: "no",
+			});
 
-		var D = process.argv[2]
-		var foodTypes = ["burger","salad","pasta","drink"];
-		var len = foodTypes.length-1;
-		for(var i =0; i<D; i++){
-			db.sendFoodToDB("BigMac"+i, //food name
-				Math.floor(Math.random() * 2 + 1), //random user id
-				"http://lorempixel.com/400/200/food/", //random photo url, check definition of the function to change it from leromPixel link to actual data
-				Math.floor(Math.random() * 100), //random price
-				Math.floor(Math.random() * 2 + 1), //random location ID
-				!!Math.floor(Math.random() * 2), !!Math.floor(Math.random() * 2), //random gluten free, and veg. parameters.
-				foodTypes[Math.floor(Math.random() * len)],//random food type from array of foodtypes
-				"amazing!")// optional test tag
-		};
+			var D = process.argv[2]
+			var foodTypes = ["burger", "salad", "pasta", "drink"];
+			var len = foodTypes.length - 1;
+			for (var i = 0; i < D; i++) {
+				db.sendFoodToDB("BigMac" + i, //food name
+					Math.floor(Math.random() * 2 + 1), //random user id
+					"http://lorempixel.com/400/200/food/", //random photo url, check definition of the function to change it from leromPixel link to actual data
+					Math.floor(Math.random() * 100), //random price
+					Math.floor(Math.random() * 2 + 1), //random location ID
+					!!Math.floor(Math.random() * 2), !!Math.floor(Math.random() * 2), //random gluten free, and veg. parameters.
+					foodTypes[Math.floor(Math.random() * len)], //random food type from array of foodtypes
+					"amazing!") // optional test tag
+			};
 
-	}
-})
+		}
+	})
 
-db.sendFoodToDB = function(food_name, 
-	user_id, 
-	photo_object, 
+db.sendFoodToDB = function(food_name,
+	user_id,
+	photo_object,
 	price,
 	location_id,
-	gFree, 
-	veg, 
-	type, 
-	tags){
-	S3.sendPhotoAndGetURL(photo_object, user_id+"/"+food_name+".jpg", function(url){
+	gFree,
+	veg,
+	type,
+	tags) {
+	S3.sendPhotoAndGetURL(photo_object, user_id + "/" + food_name + ".jpg", function(url) {
 		console.log(url);
 
 		Food.create({
@@ -140,10 +139,99 @@ db.sendFoodToDB = function(food_name,
 			type: "standart",
 			veg: veg,
 			locationId: location_id
-		}).then(()=>{
-			console.log(food_name+"  Added!")
+		}).then(() => {
+			console.log(food_name + "  Added!")
 		});
 	});
+}
+
+db.editFoodInDB = function(food_id,
+	location_id,
+	price,
+	food_name,
+	veg,
+	gFree,
+	photo_object) {
+	if (photo_object) {
+		S3.sendPhotoAndGetURL(photo_object, "pictures/" + food_name + ".jpg", function(url) {
+			console.log(url);
+
+			Food.update({
+				photoUrl: url
+			}, {
+				where: {
+					food_id: food_id
+				}
+			}).then(() => {
+				console.log(food_name + "  Updated photo!");
+			});
+		});
+	}
+	if (location_id) {
+		Food.update({
+			locationId: location_id
+		}, {
+			where: {
+				food_id: food_id
+			}
+		}).then(() => {
+			console.log(food_name + "  Updated location!");
+		});
+	}
+	if (price) {
+		Food.update({
+			price: price
+		}, {
+			where: {
+				food_id: food_id
+			}
+		}).then(() => {
+			console.log(food_name + "  Updated price!");
+		});
+	}
+	if (food_name) {
+		Food.update({
+			food_name: food_name
+		}, {
+			where: {
+				food_id: food_id
+			}
+		}).then(() => {
+			console.log(food_name + "  Updated food name!");
+		});
+	}
+	if (veg) {
+		Food.update({
+			veg: veg
+		}, {
+			where: {
+				food_id: food_id
+			}
+		}).then(() => {
+			console.log(food_name + "  Updated veg!");
+		});
+	}
+	if (gFree) {
+		Food.update({
+			gluFree: gFree
+		}, {
+			where: {
+				food_id: food_id
+			}
+		}).then(() => {
+			console.log(food_name + "  Updated gluten!");
+		});
+	}
+};
+
+db.deleteFood = function(food_id) {
+	Food.destroy({
+		where: {
+			food_id: food_id
+		}
+	}).then(() => {
+		console.log(food_id + "  Deleted!")
+	})
 }
 
 db.getFoodByType
