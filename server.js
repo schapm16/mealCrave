9c5d8aaea55b6c28dae051cd729ed585d7369ec0
Vitali Vorobyev <vvitali26@gmail.com>
2018-01-02 15:16:30 -0500

    npm packages installed

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var app = express();
 var handlebars = require("express-handlebars");
-
+const session = require('session');
+var passport       = require('passport');
+var cookieParser  = require("cookie-parser");
+var LocalStrategy  = require('passport-local').Strategy;
 //connectiong handlebars engine to express server
 app.set("view engine", "handlebars");
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
@@ -16,10 +19,30 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 //path for temporary saving photos
+app.use(cookieParser());
 app.use(bodyParser({uploadDir:'/uploads/'}));
+app.use(session({ secret: 'SECRET' }));
+
+// Passport:
+app.use(passport.initialize());
+app.use(passport.session());
 
 var controllers = require("./controllers/index.js")(app);
 
+passport.use(new LocalStrategy({
+	usernameField: 'email',
+	passwordField: 'password'
+}, function(username, password,done){
+	User.findOne({ username : username},function(err,user){
+		return err 
+		? done(err)
+		: user
+		? password === user.password
+		? done(null, user)
+		: done(null, false, { message: 'Incorrect password.' })
+		: done(null, false, { message: 'Incorrect username.' });
+	});
+}));
 
 var PORT = process.env.PORT || 8080;
 app.listen(PORT, function(){
